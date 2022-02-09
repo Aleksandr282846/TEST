90c1dfba9d8e558f0e43faa949a1f2b7295122f5
SRG <86466857+defrisk0@users.noreply.github.com>
2022-02-09 18:35:07 +0300

    Update sad.sh

--- sad.sh
+++ sad.sh
@@ -4,18 +4,24 @@ read -p "Введите название кошелька: " ADR_N
 sleep 1
 read -p "Введите пароль от кошелька: " PASS
 sleep 1
+read -p "Введите chain-id: " CHAIN
+sleep 1
+read -p "Введите название исполняемого файла (cohod, evmosd, ect.): " PR_N
+sleep 1
+read -p "Введите название токена: " TOKN
+sleep 1
 read -p "Введите время на цикл (сек.): " DELAY
 sleep 1
+
 if [ -z "$PASS" ]; then
 KB="--keyring-backend test"
 else
 KB=""
 fi
-TOKN=ucoho
-PR_N=cohod
-CHAIN=darkmatter-1 
+
 ADR_W=$(${PR_N} keys show ${ADR_N} ${KB} --output json | jq -r .address)
 ADR_V=$(${PR_N} keys show ${ADR_N} ${KB} --bech val -a)
+
 for (( ;; )); do
 echo -e "Получаем награду за делегацию\n"
 echo -e "${PASS}\ny\n" | ${PR_N} tx distribution withdraw-rewards ${ADR_V} --chain-id ${CHAIN} --from ${ADR_N} ${KB} --commission --yes
@@ -25,7 +31,7 @@ printf "Ждем %02d секунд\r" $timer
 sleep 1
 done
 BAL=$(${PR_N} q bank balances ${ADR_N} -o json | jq -r '.balances | .[].amount')
-echo -e "Баланс: ${BAL}${TOKN}\n"
+echo -e "Баланс: ${BAL}u${TOKN}\n"
 echo -e "Клеймим награды\n"
 echo -e "${PASS}\n${PASS}\n" | ${PR_N} tx distribution withdraw-all-rewards --from ${ADR_N} ${KB} --chain-id ${CHAIN} --yes
 for (( timer=20; timer>0; timer-- ))
@@ -35,12 +41,12 @@ sleep 1
 done
 BAL=$(${PR_N} q bank balances ${ADR_N} -o json | jq -r '.balances | .[].amount')
 BAL=$(($BAL-990000))
-echo -e "Баланс: ${BAL}${TOKN}\n"
+echo -e "Баланс: ${BAL}u${TOKN}\n"
 echo -e "Всю сумму отправляем в стейк\n"
 if (( BAL > 1000000 )); then
-echo -e "${PASS}\n${PASS}\n" | ${PR_N} tx staking delegate ${ADR_V}  ${BAL}${TOKN} --from ${ADR_N} ${KB} --chain-id ${CHAIN} --yes
+echo -e "${PASS}\n${PASS}\n" | ${PR_N} tx staking delegate ${ADR_V} ${BAL}u${TOKN} --from ${ADR_N} ${KB} --chain-id ${CHAIN} --yes
 else
-echo -e "Баланс: ${BAL} ${TOKN} BAL < 1000000 ${TOKN}\n"
+echo -e "Баланс: ${BAL} ${TOKN} BAL < 1 ${TOKN}\n"
 fi
 for (( timer=${DELAY}; timer>0; timer-- ))
 do
