f0a0c52d857db48e7be80a67d8c0336df1861c0b
SRG <86466857+defrisk0@users.noreply.github.com>
2022-02-13 20:23:09 +0300

    Update sad.sh

--- sad.sh
+++ sad.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #curl -s https://raw.githubusercontent.com/defrisk0/TEST/main/sad.sh > sad.sh && chmod  +x sad.sh && screen -S AD-1 './sad.sh'
-source $HOME/.bash_profile
-read -p "Введите название кошелька: " ADR_N
+read -p "Введите wallet_adress: " ADR_W
+sleep 0.5
+read -p "Введите valoper_adress: " ADR_V
 sleep 0.5
 read -p "Введите пароль от кошелька: " PASS
 sleep 0.5
@@ -11,23 +12,18 @@ read -p "Введите название исполняемого файла (co
 sleep 0.5
 read -p "Введите название токена (без u): " TOKN
 sleep 0.5
-read -p "Введите информацию для MEMO: " MEMO
-sleep 0.5
 read -p "Введите время на цикл (сек.): " DELAY
 sleep 0.5
-
+source $HOME/.bash_profile
 if [ -z "$PASS" ]; then
 KB="--keyring-backend test"
 else
 KB=""
 fi
 
-ADR_W=$(${PR_N} keys show ${ADR_N} ${KB} --output json | jq -r .address)
-ADR_V=$(${PR_N} keys show ${ADR_N} ${KB} --bech val -a)
-
 for (( ;; )); do
 echo -e "Получаем награду за делегацию:\n"
-echo -e "${PASS}\ny\n" | ${PR_N} tx distribution withdraw-rewards ${ADR_V} --chain-id ${CHAIN} --from ${ADR_N} ${KB} --note ${MEMO} --commission --yes
+echo -e "${PASS}\ny\n" | ${PR_N} tx distribution withdraw-rewards ${ADR_V} --chain-id ${CHAIN} --from ${ADR_W} ${KB} --commission --yes
 for (( timer=20; timer>0; timer-- ))
 do
 printf "Ждем %02d\r" $timer
@@ -36,7 +32,7 @@ done
 BAL=$(${PR_N} q bank balances ${ADR_W} -o json | jq -r '.balances | .[].amount')
 echo -e "Баланс: ${BAL}u${TOKN}\n"
 echo -e "Клеймим награды:\n"
-echo -e "${PASS}\n${PASS}\n" | ${PR_N} tx distribution withdraw-all-rewards --from ${ADR_N} ${KB} --chain-id ${CHAIN} --note ${MEMO} --yes
+echo -e "${PASS}\n${PASS}\n" | ${PR_N} tx distribution withdraw-all-rewards --from ${ADR_W} ${KB} --chain-id ${CHAIN} --yes
 for (( timer=20; timer>0; timer-- ))
 do
 printf "Ждем %02d\r" $timer
@@ -47,7 +43,7 @@ BAL=$(($BAL-990000))
 echo -e "Баланс: ${BAL}u${TOKN}\n"
 if (( BAL > 1000000 )); then
 echo -e "Всю сумму отправляем в стейк:\n"
-echo -e "${PASS}\n${PASS}\n" | ${PR_N} tx staking delegate ${ADR_V} ${BAL}u${TOKN} --from ${ADR_N} ${KB} --chain-id ${CHAIN} --note ${MEMO} --yes
+echo -e "${PASS}\n${PASS}\n" | ${PR_N} tx staking delegate ${ADR_V} ${BAL}u${TOKN} --from ${ADR_W} ${KB} --chain-id ${CHAIN} --yes
 else
 echo -e "Баланс: ${BAL} ${TOKN} BAL < 1 ${TOKN}\n"
 fi
