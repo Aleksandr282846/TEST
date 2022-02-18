854c13cfb0aceec189d666a3846c755353a7140c
SRG <86466857+defrisk0@users.noreply.github.com>
2022-02-18 14:39:46 +0300

    Update tbot_send.sh

--- tbot_send.sh
+++ tbot_send.sh
@@ -10,13 +10,16 @@ read -p "Введите интервал опроса (сек.): " DELAY
 sleep 0.5
 read -p "Введите название исполняемого файла (cohod, evmosd, ect.): " PR_N
 sleep 0.5
+read -p "Какой диск будем проверять: " MNT
+sleep 0.5
 for (( ;; )); do
+FS=$(df -m | grep "$MNT" | awk '{print $5}')
 VB=$(curl -s localhost:26657/status | jq -r .result.sync_info.latest_block_height)
 PJ=$(${PR_N} query staking validator ${ADD_V} --output json | jq .jailed)
 VP=$(curl -s localhost:26657/status | jq -r .result.validator_info.voting_power)
 echo -e "check, height block=${VB} | voting power=${VP}\n"
 if ([ $PJ = false ]); then
-curl -s -X POST --connect-timeout 10 "https://api.telegram.org/bot${TGAPI}/sendMessage?chat_id=${TGID}&text=✅ ${PR_N} | height block=${VB} | voting power=${VP} | jailed=${PJ}"
+curl -s -X POST --connect-timeout 10 "https://api.telegram.org/bot${TGAPI}/sendMessage?chat_id=${TGID}&text=✅ ${PR_N} | height block=${VB} | voting power=${VP} | jailed=${PJ} | use=${FS}"
 echo -e "TB SEND 1\n"
 else
 curl -s -X POST --connect-timeout 10 "https://api.telegram.org/bot${TGAPI}/sendMessage?chat_id=${TGID1}&text=❌ ${PR_N} in jail"
