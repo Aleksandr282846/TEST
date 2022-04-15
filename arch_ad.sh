0ca6752979c28f5a61b7a883545ea930da3a0a0c
SRG <86466857+defrisk0@users.noreply.github.com>
2022-04-15 15:30:34 +0300

    Update arch_ad.sh

--- arch_ad.sh
+++ arch_ad.sh
@@ -2,15 +2,16 @@
 #sudo apt update && sudo apt upgrade -y
 #sudo apt install build-essential jq wget git htop curl screen -y
 #curl -s https://raw.githubusercontent.com/defrisk0/TEST/main/arch_ad.sh > arch_ad.sh && chmod +x arch_ad.sh && screen -S SAD arch_ad.sh './sad.sh'
-read -p "\n\033[32mВведите wallet_adress (archway...): \n\033[0m" ADR_W
+read -p "Введите wallet_adress (archway...): " ADR_W
 sleep 0.2
-read -p "\n\033[32mВведите пароль от кошелька: \n\033[0m" PASS
+read -p "Введите пароль от кошелька: " PASS
 sleep 0.2
-read -p "\n\033[32mВведите время на цикл (сек.): \n\033[0m" DELAY
+read -p "Введите время на цикл (сек.): " DELAY
 sleep 0.2
 CHAIN=torii-1
 PR_N=archwayd
 TK=torii
+FS=45000
 if [ -z "$PASS" ]; then
 KB="--keyring-backend test"
 else
@@ -24,7 +25,7 @@ echo -e "\033[32mЧтобы вернуться в активную сессию 
 sleep 10
 for (( ;; )); do
 echo -e "Шаг 1 - клеймим награду за делегацию \033[32m(${ADR_V})\033[0m:\n"
-echo -e "${PASS}\ny\n" | ${PR_N} tx distribution withdraw-rewards ${ADR_V} --chain-id ${CHAIN} --from ${NAM_W} ${KB} --commission --gas auto --fees 1u${TK} --yes
+echo -e "${PASS}\ny\n" | ${PR_N} tx distribution withdraw-rewards ${ADR_V} --chain-id ${CHAIN} --from ${NAM_W} ${KB} --commission --gas auto --fees ${FS}u${TK} --yes
 for (( timer=15; timer>0; timer-- ))
 do
 printf "Пауза %02d \r" $timer
@@ -34,7 +35,7 @@ BAL=$(${PR_N} q bank balances ${ADR_W} -o json | jq -r '.balances | .[].amount')
 echo -e "\n"
 echo -e "Проверяем баланс. Баланс: ${BAL}u${TOKN}\n"
 echo -e "Шаг 2 - клеймим награды:\n"
-echo -e "${PASS}\ny\n" | ${PR_N} tx distribution withdraw-all-rewards --from ${NAM_W} ${KB} --chain-id ${CHAIN} --gas auto --fees 1u${TK} --yes
+echo -e "${PASS}\ny\n" | ${PR_N} tx distribution withdraw-all-rewards --from ${NAM_W} ${KB} --chain-id ${CHAIN} --gas auto --fees ${FS}u${TK} --yes
 for (( timer=15; timer>0; timer-- ))
 do
 printf "Пауза %02d \r" $timer
@@ -45,7 +46,7 @@ BAL=$(($BAL-99000))
 echo -e "\n"
 if ((BAL > 100000)); then
 echo -e "Делегируем всю сумму:\n"
-echo -e "${PASS}\ny\n" | ${PR_N} tx staking delegate ${ADR_V} ${BAL}u${TOKN} --from ${NAM_W} ${KB} --chain-id ${CHAIN} --gas auto --fees 1u${TK} --yes
+echo -e "${PASS}\ny\n" | ${PR_N} tx staking delegate ${ADR_V} ${BAL}u${TOKN} --from ${NAM_W} ${KB} --chain-id ${CHAIN} --gas auto --fees ${FS}u${TK} --yes
 else
 echo -e "Баланс ${BAL} u${TK} меньше безопасного значения, собираем дальше.\n"
 fi
