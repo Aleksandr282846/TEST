9da6e94554c28cb070e80c892044b20a1b9b97ca
SRG <86466857+defrisk0@users.noreply.github.com>
2022-04-26 21:00:05 +0300

    Update sad_tn.sh

--- sad_tn.sh
+++ sad_tn.sh
@@ -28,7 +28,7 @@ CK=$(echo "($CK + 0.5)/1" | bc)
 CR=$(${PR_N} query distribution rewards ${ADR_W} ${ADR_V} -o json | jq -r  .rewards[].amount)
 CR=$(echo "($CR + 0.5)/1" | bc)
 SN=$(($CK+$CR))
-echo -e "\033[32mПроверка суммы. Комиссия ${CK}u${TK} + реварды ${CR}u${TK} = $(({SN}/1000000))${TK}\033[0m"
+echo -e "\033[32mПроверка суммы. Комиссия ${CK}u${TK} + реварды ${CR}u${TK} = $((${SN}/1000000))${TK}\033[0m"
 if ((SN > DR)); then
 echo -e "\033[32mШаг 1 - клеймим награду за делегацию \033[31m(${ADR_V})\033[0m:\n"
 echo -e "${PASS}\ny\n" | ${PR_N} tx distribution withdraw-rewards ${ADR_V} --chain-id ${CHAIN} --from ${NAM_W} ${KB} --commission --gas auto --fees ${FS}u${TK} --yes
