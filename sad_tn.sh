e53bd1b60dc2e38c16f687c964a6a158b1a487da
SRG <86466857+defrisk0@users.noreply.github.com>
2022-04-26 21:07:48 +0300

    Update sad_tn.sh

--- sad_tn.sh
+++ sad_tn.sh
@@ -27,7 +27,7 @@ CK=$(${PR_N} query distribution commission ${ADR_V} -o json | jq -r  .commission
 CK=$(echo "($CK + 0.5)/1" | bc)
 CR=$(${PR_N} query distribution rewards ${ADR_W} ${ADR_V} -o json | jq -r  .rewards[].amount)
 CR=$(echo "($CR + 0.5)/1" | bc)
-SN=$(($CK+$CR))
+SN=$(($CK + $CR))
 echo -e "\033[32mПроверка суммы. Комиссия ${CK}u${TK} + реварды ${CR}u${TK} = $((${SN}/1000000))${TK}\033[0m"
 if ((SN > DR)); then
 echo -e "\033[32mШаг 1 - клеймим награду за делегацию \033[31m(${ADR_V})\033[0m:\n"
@@ -47,7 +47,7 @@ done
 BAL=$(${PR_N} q bank balances ${ADR_W} -o json | jq -r '.balances | .[].amount')
 echo -e "\033[32mПроверяем баланс. Баланс: ${BAL}u${TK}\033[0m\n"
 sleep 1
-BAL=$(($BAL-990000))
+BAL=$(($BAL - 990000))
 if ((BAL > 1000000)); then
 echo -e "\033[32mШаг 3. Делегируем всю сумму:\033[0m\n"
 echo -e "${PASS}\ny\n" | ${PR_N} tx staking delegate ${ADR_V} ${BAL}u${TK} --from ${NAM_W} ${KB} --chain-id ${CHAIN} --gas auto --fees ${FS}u${TK} --yes
