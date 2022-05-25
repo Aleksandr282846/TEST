80eb6959d861001b7b3f5cc4c53c097257a45b16
SRG <86466857+defrisk0@users.noreply.github.com>
2022-05-25 12:29:44 +0300

    Update sbsp.sh

--- sbsp.sh
+++ sbsp.sh
@@ -4,13 +4,15 @@ read -p "Введите название ноды: " NM_N
 sleep 0.5
 read -p "Введите адрес на который будем фармить: " ADR_W
 sleep 0.5
+read -p "Введите размер PLOT_SIZE (например 100G): " PS
+sleep 0.5
 sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential jq wget git htop curl -y
 cd $HOME
 rm -rf subspace*
 sleep 0.5
-wget -O subspace-node https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-node-ubuntu-x86_64-snapshot-2022-mar-09
-wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/snapshot-2022-may-19/subspace-node-ubuntu-x86_64-snapshot-2022-may-19
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/snapshot-2022-may-19/subspace-farmer-ubuntu-x86_64-snapshot-2022-may-19
 chmod +x subspace*
 mv subspace* /usr/local/bin/
 
@@ -21,7 +23,7 @@ After=network.target
 [Service]
 User=$USER
 Type=simple
-ExecStart=$(which subspace-node) --chain testnet --wasm-execution compiled --execution wasm --bootnodes \"/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr\" --rpc-cors all --rpc-methods unsafe --ws-external --validator --telemetry-url \"wss://telemetry.polkadot.io/submit/ 1\" --name $NM_N
+ExecStart=$(which subspace-node) --chain testnet --execution wasm -unsafe-pruning --pruning 1024 --keep-blocks 1024 --validator --name $NM_N
 Restart=on-failure
 LimitNOFILE=65535
 
@@ -35,7 +37,7 @@ After=network.target
 [Service]
 User=$USER
 Type=simple
-ExecStart=$(which subspace-farmer) farm --reward-address $ADR_W
+ExecStart=$(which subspace-farmer) farm --reward-address $ADR_W --plot-size $PS
 Restart=on-failure
 LimitNOFILE=65535
 
