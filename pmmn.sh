472b9e85899065501b4b241580ee9d88f454ab15
SRG <86466857+defrisk0@users.noreply.github.com>
2022-09-21 16:56:00 +0300

    Update pmmn.sh

--- pmmn.sh
+++ pmmn.sh
@@ -1,47 +1,22 @@
 #!/bin/bash
-AD_N=1
-read -p "Введите адрес: " AD_A
-sleep 1
-
-# if any params recived
-if [ "$#" -gt 0 ]; then
-if [ "$1" != "-file" ]; then
-echo "Нажмите клашину [ -file <file_location> ] для указания пути к файлу. (1)"
-exit	#exit if wrong key
-fi
-if [ "$2" = "" ]; then
-echo "Укажите путь в файлу."
-exit	#exit if now file name
-else
-file_path=$2	#file path
-if [ ! -f "$file_path" ]; then
-echo "$file_path does not exist."
-fi
-fi
-#read file by lines
-while read line; do
-FIND=$line	#value from file's line
+# wget https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt
+PR=1
+ADD=
+DT=$(date)
 for (( ;; )); do
-echo -e "Проверка слова ${FIND}"
-MNEM=" $FIND"
-echo -e "${MNEM}\ny\n" | cohod keys add ${AD_N} --recover --keyring-backend test --output json | jq -r .address
-SS=$(cohod keys show ${AD_N} --keyring-backend test --output json | jq -r .address)
+PS=$(sed -n ${PR}p english.txt)
+MNEM="half decide tent pave ${PS} rose card goddess potato brush merry indicate balcony skate course cotton weasel visa slice rice stay parent nose gap"
+echo -e ${PS}
+echo -e "${MNEM}" | haqqd keys add TEST --recover --keyring-backend test
+SS=$(haqqd keys show TEST --keyring-backend test --output json | jq -r .address)
 echo -e ${SS}
-if [[ $SS = $AD_A ]]; then
-echo -e "Необходимое слово ${FIND}"
+echo -e ${DT} ${SS} >> log.txt
+echo -e ${MNEM}
+echo -e ${DT} ${MNEM} >> log.txt
+if [[ ${SS} = ${ADD} ]]; then
 break
 else
-echo -e "${SS} не является ${AD_A}"
-cohod keys delete ${AD_N} --keyring-backend test --yes
-AD_N=$(($AD_N+1))
+haqqd keys delete TEST --keyring-backend test -y
+PR=$((${PR}+1))
 fi
-for (( timer=5; timer>0; timer-- )); do
-printf "Ждем %02d секунд\r" $timer
-sleep 1
 done
-done
-done < $file_path
-
-else
-	echo "Please, use key [ -file <file_location> ] to specify path to text file."
-fi
